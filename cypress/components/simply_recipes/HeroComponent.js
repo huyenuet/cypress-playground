25df2b176924435d810bd00e88d02f0ee6d9c010
huyen tran <thanhhuyenk59uet@gmail.com>
2023-12-10 19:24:12 +0700

    chore: convert file type from ts to js

---
+++ cypress/components/simply_recipes/HeroComponent.js
@@ -0,0 +1,18 @@
+export class HeroComponent {
+    static CONTAINER_SEL = '.showcase-hero';
+    
+    // TODO: change the syntax to Typescript
+    constructor(container) {
+        this.container = container;
+    }
+    
+    getCardTitle() {
+        let cardTitle = '';
+        this.container.find(".card__title").then($title => {
+            cardTitle = $title.text().trim()
+        })
+        // I have no idea what I'm writing
+        return new Cypress.Promise(resolve => cy.wrap('').then(() => resolve(cardTitle)))
+    }
+
+}
