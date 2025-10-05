db790c2c412d2e19b820afdfea6f2381f11a5f8d
huyen tran <thanhhuyenk59uet@gmail.com>
2025-10-05 10:11:00 +0700

    chore: remove Amazon tests

--- cypress/models/pages/AmazonSearchResultPage.ts
+++
@@ -1,7 +0,0 @@
-const RESULT_ITEM_SEL = 'div[cel_widget_id^="MAIN-SEARCH_RESULTS"]'
-
-export class AmazonSearchResultPage {
-    get searchItemElemList() {
-        return cy.get(RESULT_ITEM_SEL)
-    }
-}
