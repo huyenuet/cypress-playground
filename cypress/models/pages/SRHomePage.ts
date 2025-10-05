fa4a831abb60ca31a434a75da3e1f006999482ab
huyen tran <thanhhuyenk59uet@gmail.com>
2025-10-05 10:20:33 +0700

    chore: remove irrelevent tests

--- cypress/models/pages/SRHomePage.ts
+++
@@ -1,7 +0,0 @@
-import { HeroComponent } from "../components/HeroComponent";
-
-export class SRHomePage {
-    get heroComponent() {
-        return new HeroComponent(cy.get(HeroComponent.CONTAINER_SEL))
-    }
-}
